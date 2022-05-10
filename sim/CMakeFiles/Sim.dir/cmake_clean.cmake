1d6c911c07223e2e6114a7bb29261aaa56a85a34
maximilianKoeper <16453175+maximilianKoeper@users.noreply.github.com>
2022-05-10 14:07:54 +0200

    initial geant4 test

---
+++ sim/CMakeFiles/Sim.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sim.dir/sim.cc.o"
+  "CMakeFiles/Sim.dir/sim.cc.o.d"
+  "CMakeFiles/Sim.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/Sim.dir/src/ActionInitialization.cc.o.d"
+  "CMakeFiles/Sim.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/Sim.dir/src/DetectorConstruction.cc.o.d"
+  "CMakeFiles/Sim.dir/src/EventAction.cc.o"
+  "CMakeFiles/Sim.dir/src/EventAction.cc.o.d"
+  "CMakeFiles/Sim.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/Sim.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "CMakeFiles/Sim.dir/src/RunAction.cc.o"
+  "CMakeFiles/Sim.dir/src/RunAction.cc.o.d"
+  "CMakeFiles/Sim.dir/src/SteppingAction.cc.o"
+  "CMakeFiles/Sim.dir/src/SteppingAction.cc.o.d"
+  "Sim"
+  "Sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sim/CMakeFiles/Sim.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Sim.
+# This may be replaced when dependencies are built.
