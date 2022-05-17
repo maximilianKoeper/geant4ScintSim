b1c9320609d57209ef00ded8f199405de66f00a1
maximilianKoeper <16453175+maximilianKoeper@users.noreply.github.com>
2022-05-17 13:42:10 +0200

    renaming namespace and cleanup

--- sim/include/RunAction.hh
+++ sim/include/RunAction.hh
@@ -1,34 +1,9 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-//
-/// \file RunAction.hh
-/// \brief Definition of the B1::RunAction class
+/*
+ * RunAction.hh
+ */
 
-#ifndef B1RunAction_h
-#define B1RunAction_h 1
+#ifndef RunAction_h
+#define RunAction_h 1
 
 #include "G4UserRunAction.hh"
 #include "G4Accumulable.hh"
@@ -42,7 +17,7 @@ class G4Run;
 /// from the energy deposit accumulated via stepping and event actions.
 /// The computed dose is then printed on the screen.
 
-namespace B1
+namespace sim
 {
 
 class RunAction : public G4UserRunAction
@@ -64,4 +39,3 @@ class RunAction : public G4UserRunAction
 }
 
 #endif
-
